2f0c58744b9db958fb9826fbcebf0786753346aa
AceDroidX <15519693+AceDroidX@users.noreply.github.com>
2021-12-25 16:38:14 +0800

    feat: 🎸 添加API:通过时间戳获取SC列表

--- packages/superspider-mcore/api/sc.js
+++ packages/superspider-mcore/api/sc.js
@@ -3,7 +3,7 @@ const sc = new Router()
 
 const giftConv = require('../utils/giftConv')
 
-const log = process.env.NODE_ENV == 'development' ? console.log : () => { }
+const log = process.env.NODE_ENV == 'development' ? console.log : () => {}
 
 const mergeTime = 30000005
 
@@ -126,6 +126,67 @@ sc.post('/getData', async (ctx, next) => {
       await next()
       console.log(e)
     }
+  }
+})
+
+// /sc/getDataByTS
+// 通过时间戳获取SC列表
+// 参数：
+//   roomid: 房间号
+//   start: 开始时间戳
+//   end: 结束时间戳
+sc.post('/getDataByTS', async (ctx, next) => {
+  if (!global.amdb) {
+    ctx.response.status = 500
+    ctx.response.body = 'Internal Server Error: AMDB ERR'
+    await next()
+    return
+  }
+  if (!global.predb) {
+    ctx.response.status = 500
+    ctx.response.body = 'Internal Server Error: PreDB ERR'
+    await next()
+    return
+  }
+  const amdb = global.amdb
+  const predb = global.predb
+  switch (true) {
+    case !ctx.request.body.roomid || isNaN(Number(ctx.request.body.roomid)):
+    case !ctx.request.body.start || isNaN(Number(ctx.request.body.start)):
+    case !ctx.request.body.end || isNaN(Number(ctx.request.body.end)):
+      ctx.response.status = 404
+      ctx.response.body = 'Bad Request Format'
+      await next()
+      return
+  }
+  try {
+    const roomid = Number(ctx.request.body.roomid)
+    const start = Number(ctx.request.body.start)
+    const end = Number(ctx.request.body.end)
+    log(`LOG req in rid ${roomid}`)
+    const finded = await amdb
+      .find({ roomid: roomid, ts: { $gte: start, $lte: end } })
+      .sort('ts', -1)
+      .limit(1000)
+      .toArray()
+    log(`LOG amdb complete ${roomid}`)
+    if (finded.length >= 1000) {
+      log(`LOG amdb Too Many Results(>=1000)${start}-${end} ${roomid}`)
+      ctx.response.status = 403
+      ctx.response.body = 'Too Many Results(>=1000)'
+      await next()
+      return
+    }
+    ctx.response.status = 200
+    ctx.response.body = finded
+    log(`LOG next start ${roomid}`)
+    await next()
+    log(`LOG next end ${roomid}`)
+  } catch (e) {
+    ctx.response.status = 500
+    ctx.response.body = 'Internal Server Error'
+    await next()
+    console.log(e)
   }
 })
 
