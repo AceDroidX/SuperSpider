6606f265e4fe762fa1517d0b11f746c12785b3fe
AceDroidX <15519693+AceDroidX@users.noreply.github.com>
2023-02-12 11:32:15 +0000

    Deploying to gh-pages from @ AceDroidX/SuperSpider@2be8998f02ac6a06a83c3134f02dd72ecffa253d 🚀

---
+++ _nuxt/VCardText.e284fb4f.js
@@ -0,0 +1,1 @@
+import{c as t}from"./createSimpleFunctional.5cc6fc0d.js";const e=t("v-card-text");export{e as V};
---
+++ _nuxt/tag.7f81d670.js
@@ -0,0 +1,1 @@
+import{S as r,L as a}from"./entry.2cf9485d.js";function s(e){const t=r("useRender");t.render=e}const o=a({tag:{type:String,default:"div"}},"tag");export{o as m,s as u};
